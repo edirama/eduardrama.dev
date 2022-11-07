578a414c38623115839a15b4950533044b1d7cb6
edirama <er51753@ubt-uni.net>
2022-11-07 19:54:53 +0100

    smoth-scroll && sticky-navbar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 // NavBar
 const headerBars = document.querySelector('.header--bars')
 const navbarList = document.querySelector('.navbar--list')
+const navBar = document.querySelector('#navbar')
+const navBarLinks = document.querySelectorAll('.navbar--link')
 
-headerBars.addEventListener('click', function () {
+const toggleNavBar = function () {
   headerBars.classList.toggle('active')
   navbarList.classList.toggle('navbar--active')
+}
+
+headerBars.addEventListener('click', toggleNavBar)
+
+navBarLinks.forEach(item => {
+  item.addEventListener('click', toggleNavBar)
 })
+
+document.addEventListener('scroll', function (e) {
+  scrollY > 740
+    ? navBar.classList.add('sticky')
+    : navBar.classList.remove('sticky')
+})
+
+for (let i = 0; i > navBarLinks.length; i++) {
+  navBarLinks[i].addEventListener('click', toggleNavBar)
+}
